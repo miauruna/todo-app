a14dbdc8f059410794e563a5a1107197033741a6
Pupincea <littledr3am@gmail.com>
2022-06-11 21:25:19 +0200

    added a sign out button - unstyled

--- src/Components/Pages/List/ListPage.jsx
+++ src/Components/Pages/List/ListPage.jsx
@@ -3,12 +3,21 @@ import { useNavigate } from 'react-router-dom';
 import UserContext from '../../Store/Auth/UserContext';
 import TodoList from '../../Organisms/TodoList/TodoList';
 import './ListPage.css';
+import Button from '../../Atoms/Button/Button';
+
 const ListPage = () => {
-	const { userData } = useContext(UserContext);
+	const { userData, setUserData } = useContext(UserContext);
 	const navigate = useNavigate();
 	useEffect(() => {
 		if (!userData.user) navigate('/login');
 	}, [userData.user, navigate]);
+	const logout = () => {
+		setUserData({
+			token: undefined,
+			user: undefined,
+		});
+		localStorage.setItem('auth-token', '');
+	};
 	return (
 		<div className='list-page-wrapper'>
 			{' '}
@@ -16,6 +25,12 @@ const ListPage = () => {
 				<>
 					Welcome, {userData.user.displayName}! We're glad to have you
 					back.
+					<Button
+						className='sign-out-button'
+						type='button'
+						onClick={logout}>
+						Sign Out
+					</Button>
 					<TodoList />{' '}
 				</>
 			) : (
